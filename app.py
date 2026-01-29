bbac77c809f39676251fdb7c651def4901130d0c
Prabhu Kasinathan <pkasinathan@netflix.com>
2026-01-28 20:40:11 -0800

    Update Streamlit app config for 'I Survived 99 Nights'

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="NEON SURVIVOR | 2087",
-    page_icon="🎮",
+    page_title="I Survived 99 Nights",
+    page_icon="🌙",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
